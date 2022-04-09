e3e2e2abc584b4a61d640c639b146d0f5f1ffcc7
jacques <jacques.perrault@branch.technology>
2022-04-09 12:04:27 -0400

    moved func to struct

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -4,43 +4,49 @@ use std::time::Duration;
 use std::error::Error;
 
 
-async fn connection_rumqtt(){
-    let mut mqttoptions = MqttOptions::new("rumqtt-test", "localhost", 1883);
-    mqttoptions.set_keep_alive(Duration::from_secs(5));
 
-    let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-    client.subscribe("test", QoS::AtMostOnce).await.unwrap_or_else(|e|{
-        println!("failed to subscribe {:?}", e);
-    });
+struct MqttConnection {
+    client_id: String,
+    host: String,
+    port: u16,
+    topic: String,
+}
 
-    // task::spawn(async move {
-    //     for i in 0..10 {
-    //         client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
-    //         time::sleep(Duration::from_millis(100)).await;
-    //     }
-    // });
 
-    loop {
-        let event = eventloop.poll().await;
-        println!("{:?}", event);
-        match eventloop.poll().await {
-            Ok(Event::Incoming(Incoming::Publish(p))) => {
-                println!("Topic: {}, Payload: {:?}", p.topic, p.payload);
-            }
-            Ok(Event::Incoming(i)) => {
-                println!("Incoming = {:?}", i);
-            }
-            Ok(Event::Outgoing(o)) => println!("Outgoing = {:?}", o),
-            Err(e) => {
-                println!("Error = {:?}", e);
+impl MqttConnection {
+    async fn connection_rumqtt(&self){
+        let mut mqttoptions = MqttOptions::new(&self.client_id, &self.host, self.port);
+        mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+        let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+        client.subscribe(&self.topic, QoS::AtMostOnce).await.unwrap_or_else(|e|{
+            println!("failed to subscribe {:?}", e);
+        });
+
+        // task::spawn(async move {
+        //     for i in 0..10 {
+        //         client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+        //         time::sleep(Duration::from_millis(100)).await;
+        //     }
+        // });
+
+        loop {
+            let event = eventloop.poll().await;
+            println!("{:?}", event);
+            match eventloop.poll().await {
+                Ok(Event::Incoming(Incoming::Publish(p))) => {
+                    println!("Topic: {}, Payload: {:?}", p.topic, p.payload);
+                }
+                Ok(Event::Incoming(i)) => {
+                    println!("Incoming = {:?}", i);
+                }
+                Ok(Event::Outgoing(o)) => println!("Outgoing = {:?}", o),
+                Err(e) => {
+                    println!("Error = {:?}", e);
+                }
             }
         }
     }
-
-    // loop {
-    //     let notification = eventloop.poll().await.unwrap();
-    //     println!("Received = {:?}", notification);
-    // }
 }
 
 
@@ -50,6 +56,13 @@ mod test_eval{
 
     #[tokio::test]
     async fn test_control_construct() {
-        connection_rumqtt().await;
+        // connection_rumqtt().await;
+        let client = MqttConnection{
+            client_id: "test_client".to_string(),
+            host: "localhost".to_string(),
+            port: 1883,
+            topic: "test".to_string()
+        };
+        client.connection_rumqtt().await;
     }
 }
