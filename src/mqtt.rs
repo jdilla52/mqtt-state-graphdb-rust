dcc50a79356d6f32183133258da7fee1726d7e40
jacques <jacques.perrault@branch.technology>
2022-04-09 11:51:14 -0400

    thread loop appears to work

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,4 +1,4 @@
-use rumqttc::{MqttOptions, AsyncClient, QoS};
+use rumqttc::{MqttOptions, AsyncClient, QoS, Event, Incoming};
 use tokio::{task, time};
 use std::time::Duration;
 use std::error::Error;
@@ -9,20 +9,32 @@ async fn connection_rumqtt(){
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
     let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-    client.subscribe("hello/rumqtt", QoS::AtMostOnce).await.unwrap_or_else(|e|{
+    client.subscribe("test", QoS::AtMostOnce).await.unwrap_or_else(|e|{
         println!("failed to subscribe {:?}", e);
     });
 
-    task::spawn(async move {
-        for i in 0..10 {
-            client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
-            time::sleep(Duration::from_millis(100)).await;
-        }
-    });
+    // task::spawn(async move {
+    //     for i in 0..10 {
+    //         client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+    //         time::sleep(Duration::from_millis(100)).await;
+    //     }
+    // });
 
     loop {
-        let notification = eventloop.poll().await.unwrap();
-        println!("Received = {:?}", notification);
+        let event = eventloop.poll().await;
+        println!("{:?}", event);
+        match eventloop.poll().await {
+            Ok(Event::Incoming(Incoming::Publish(p))) => {
+                println!("Topic: {}, Payload: {:?}", p.topic, p.payload);
+            }
+            Ok(Event::Incoming(i)) => {
+                println!("Incoming = {:?}", i);
+            }
+            Ok(Event::Outgoing(o)) => println!("Outgoing = {:?}", o),
+            Err(e) => {
+                println!("Error = {:?}", e);
+            }
+        }
     }
 
     // loop {
