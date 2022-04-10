a78e2edea73245519f6957ac052912139f96cf0b
jacques <jacques.perrault@branch.technology>
2022-04-10 16:27:57 -0400

    generates path from topic

--- src/graph_db.rs
+++ src/graph_db.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use log::debug;
 use neo4rs::{Graph, Node, query};
 use crate::config::GraphdbSettings;
 
@@ -48,9 +49,34 @@ impl Graphdb {
     pub async fn create_path(&self, topic: String, data: String){
         let mut txn = self.graph.start_txn().await.unwrap();
 
-        txn.run(query( "MERGE (n1 :Person {name: '1'})\
-                               MERGE (n2 :Person {name: '2'})\
-                               MERGE (n1) <-[:SUB]- (n2)"))
+
+        let merges: Vec<&str> = topic.split("/").collect();
+        let mut pattern = "MERGE (root :ROOT {name: 'root'})".to_string(); // root
+
+        // this will result in duplicate dodes where if a user places a value halfway through the path.
+        // I think this is fine and should make queries substantially faster as we can just grab all values.
+
+        let m0 = merges[0];
+        if merges.len() == 1{
+            pattern = pattern + &format!(" MERGE ({v} :Value {{name: '{v}'}})", v = m0); // first node
+            pattern = pattern + &format!(" MERGE (root) <-[:SUB]- ({v})", v=m0); // connect two above
+        }
+        else {
+            pattern =pattern + &format!(" MERGE ({v} :Path {{subpath: '{v}'}})", v=m0); // first node
+            pattern =pattern + &format!(" MERGE (root) <-[:SUB]- ({v})", v=m0); // connect two above
+        
+            for (i, v) in (&merges[1..merges.len()-1]).iter().enumerate(){
+                pattern = pattern + &format!(" MERGE ({v} :Path {{subpath: '{v}'}})", v=v);
+                pattern = pattern + &format!(" MERGE ({v0}) <-[:SUB]- ({v1})", v0=v, v1=merges[i]); // connect current to previous
+            }
+            let ml = merges.last().unwrap();
+            pattern = pattern + &format!(" MERGE ({v} :Value {data})", v = ml, data=data); // first node // TODO add actual value
+            pattern = pattern + &format!(" MERGE ({v0}) <-[:SUB]- ({v1})", v0=merges[merges.len()-2], v1=ml); // connect
+        }
+
+        debug!("pattern: {}", pattern);
+
+        txn.run(query( pattern.as_str()))
             .await
             .unwrap();
         txn.commit().await.unwrap(); //or txn.rollback().await.unwrap();
@@ -65,10 +91,19 @@ mod graph {
     use crate::graph_db::{example_query, example_transaction, Graphdb};
 
     #[tokio::test]
-    async fn test_create_path() {
+    async fn test_create_single_path() {
         let client = Graphdb::new(GraphdbSettings::default()).await;
-        client.create_path("test/tes2/test_value".to_string(), "hello".to_string()).await;
+        // test single path
+        client.create_path("test".to_string(), "hello".to_string()).await;
     }
+
+    #[tokio::test]
+    async fn test_create_deep_path() {
+        let client = Graphdb::new(GraphdbSettings::default()).await;
+        // test single path
+        client.create_path("test/test2/hello".to_string(), "{name: 'value', test_val:'hello'}".to_string()).await;
+    }
+
     #[tokio::test]
     async fn transaction () {
         let uri = "127.0.0.1:7687";
