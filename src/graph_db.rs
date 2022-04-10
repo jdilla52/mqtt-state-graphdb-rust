81c39e95a91abe2749614f271b137dda2ebbb0c8
jacques <jacques.perrault@branch.technology>
2022-04-10 16:06:11 -0400

    add path working with merges

--- src/graph_db.rs
+++ src/graph_db.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 use neo4rs::{Graph, Node, query};
+use crate::config::GraphdbSettings;
 
 async fn example_query(graph: Arc<Graph>){
     for _ in 1..=42 {
@@ -30,12 +31,44 @@ async fn example_transaction(graph: Arc<Graph>){
     txn.commit().await.unwrap(); //or txn.rollback().await.unwrap();
 }
 
+pub struct Graphdb {
+    settings: GraphdbSettings,
+    graph: Arc<Graph>
+}
+
+impl Graphdb {
+    pub async fn new(settings: GraphdbSettings) -> Graphdb {
+        let graph = Arc::new(Graph::new(&settings.address, &settings.user, &settings.pass).await.unwrap());
+        Graphdb{
+            settings,
+            graph
+        }
+    }
+
+    pub async fn create_path(&self, topic: String, data: String){
+        let mut txn = self.graph.start_txn().await.unwrap();
+
+        txn.run(query( "MERGE (n1 :Person {name: '1'})\
+                               MERGE (n2 :Person {name: '2'})\
+                               MERGE (n1) <-[:SUB]- (n2)"))
+            .await
+            .unwrap();
+        txn.commit().await.unwrap(); //or txn.rollback().await.unwrap();
+    }
+}
+
 #[cfg(test)]
 mod graph {
     use std::sync::Arc;
     use neo4rs::{Graph, query};
-    use crate::graph_db::{example_query, example_transaction};
+    use crate::config::GraphdbSettings;
+    use crate::graph_db::{example_query, example_transaction, Graphdb};
 
+    #[tokio::test]
+    async fn test_create_path() {
+        let client = Graphdb::new(GraphdbSettings::default()).await;
+        client.create_path("test/tes2/test_value".to_string(), "hello".to_string()).await;
+    }
     #[tokio::test]
     async fn transaction () {
         let uri = "127.0.0.1:7687";
